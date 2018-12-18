f749e53fb6b600965c367205ff8eba023f79668d
ponyaid <moskvichovoleg@gmail.com>
2018-12-18 20:34:49 +0400

    Solution 'Hard Work'

---
+++ hard_work.py
@@ -0,0 +1,15 @@
+import re
+
+
+def alpha(x):
+    return ''.join([i for i in x if i.isalpha()])
+
+
+w1, w2, w3 = alpha(input().lower()), alpha(input().lower()), alpha(input().lower())
+r = r'(%s)|(%s)|(%s)' % (w1, w2, w3)
+for i in range(int(input())):
+    a = alpha(input()).lower()
+    print('WA' if re.sub(r, '', a) else 'ACC')
+
+
+
